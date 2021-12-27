18d14312f88cdca56b2963c7f3f2ebbf14ec3002
notjoemartinez <39905973+NotJoeMartinez@users.noreply.github.com>
2021-12-27 16:42:13 -0600

    unsafe string conncatination for sql queary but it does the job

--- tests/sqlite3_tests/make_db.c
+++ tests/sqlite3_tests/make_db.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
 #include <sqlite3.h>
+#include <string.h>
 
 int make_db();
 int read_db();
-int update_db();
+int update_db(char path[100], char desc[100]);
 int test_db();
 
 int main(){
-	make_db();
+	// make_db();
+    // test_db();
     test_db();
 	return 0;
 }
 
 int test_db(){
 
-    static struct path_map = {
-    "/Users/supernova/code/c_shit/journal_app/tests/cli_stuff"
-    "/Users/supernova/code/c_shit/journal_app/tests/external_editing"
-    "/Users/supernova/code/c_shit/journal_app/tests/importing_stuff"
-    "/Users/supernova/code/c_shit/journal_app/tests/sqlite3_tests"
+    char paths[10][100][100]= {
+        {"~/code/c_shit/joe_jrnl/tests/cli_stuff", "Commanline Testing"},
+        {"~/code/c_shit/joe_jrnl/tests/external_editing", "External Editor Testing"},
+        {"~/code/c_shit/joe_jrnl/tests/importing_stuff", "Import Statment testing"},
+        {"~/code/c_shit/joe_jrnl/tests/sqlite3_tests", "Database Testing"},
+        };
+    
+    for (int i=0; i<=3; i++){
+        update_db(paths[i][0],paths[i][1]);
     }
+
+    return 0;
 }
 
 int make_db(){
@@ -58,10 +66,11 @@ int make_db(){
 
 }
 
-int update_db(){
+int update_db(char fpath[100], char desc[100]){
  	sqlite3 *db;
 	char *err_msg = 0;
 
+    printf("%s, %s\n", fpath, desc);
 	int rc = sqlite3_open("jrnl.db", &db);
 
 	// making sure the db was created
@@ -73,9 +82,15 @@ int update_db(){
         return 1;
     }
 
-	char *sql = "INSERT INTO joejrnl VALUES('','')";
+    char query[512];
+	char *sql = "INSERT INTO joejrnl VALUES(\'%s\',\'%s\');";
+    sprintf(query, sql, fpath, desc);
+    printf("%s\n",query);
 
-	rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
+
+
+
+	rc = sqlite3_exec(db, query, 0, 0, &err_msg);
 
 	// sql quearies might fail check for erros and close db 
     if (rc != SQLITE_OK ) {
